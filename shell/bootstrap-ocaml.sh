260eecf9120641b3d57664427b1aab7771e4c52a
David Allsopp <david.allsopp@metastack.com>
2015-07-22 10:52:40 +0100

    Support wget or curl in bootstrap-ocaml.sh

--- shell/bootstrap-ocaml.sh
+++ shell/bootstrap-ocaml.sh
@@ -2,10 +2,15 @@
 
 V=ocaml-4.02.1
 URL=http://caml.inria.fr/pub/distrib/ocaml-4.02/${V}.tar.gz
+if command -v curl > /dev/null ; then
+  CURL="curl -OL"
+else
+  CURL=wget
+fi
 mkdir -p bootstrap
 cd bootstrap
 if [ ! -e ${V}.tar.gz ]; then
-  curl -OL ${URL}
+  ${CURL} ${URL}
 fi
 tar -zxf ${V}.tar.gz
 cd ${V}
@@ -90,7 +95,7 @@ if [ -n "$1" -a -n "${COMSPEC}" -a -x "${COMSPEC}" ] ; then
   FV=0.34
   cd ..
   if [ ! -e flexdll-bin-${FV}.zip ]; then
-    curl -OL http://alain.frisch.fr/flexdll/flexdll-bin-${FV}.zip
+    ${CURL} http://alain.frisch.fr/flexdll/flexdll-bin-${FV}.zip
   fi
   mkdir -p bin
   unzip -od bin/ flexdll-bin-${FV}.zip
